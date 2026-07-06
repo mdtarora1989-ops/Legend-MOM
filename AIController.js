e8302df7bcc93a915e9cd6d9771cfc9f269ce471
Mudit Arora <mudit.arora@legendpolyfoams.com>
2026-07-06 11:55:24 +0530

    B3 Response Validator

--- AIController.js
+++ AIController.js
@@ -13,3 +13,26 @@ function openAIAssistant() {
 function include(filename) {
   return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
+function validateAIResponse(text) {
+  text = text.trim();
+
+  text = text.replace(/^```json/i, "");
+
+  text = text.replace(/^```/i, "");
+
+  text = text.replace(/```$/, "");
+
+  const obj = JSON.parse(text);
+
+  return {
+    message: "JSON Valid",
+
+    preview: JSON.stringify(obj, null, 2),
+  };
+}
+
+function insertValidatedResponse() {
+  SpreadsheetApp.getUi()
+
+    .alert("Insert Pipeline will be connected in B4.");
+}
